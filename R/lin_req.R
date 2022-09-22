211db8642c0c473b9183cc60467d50a1b32b7faf
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-09-23 01:28:11 +0200

    doing linreq

--- R/lin_req.R
+++ R/lin_req.R
@@ -1,1 +1,7 @@
-linreq <- function()
+
+linreq <- function(formula, data) {
+  LinreqClass <- setRefClass("linreq", fields = list(balance = "numeric"))
+  a <- LinreqClass$new(balance = 100)
+  return(a)
+}
+linreq(4,5)
