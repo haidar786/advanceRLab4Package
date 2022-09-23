c2f4e8b7782f78af97b395e932df68a5e410b121
Hussnain Haidar <23493900+haidar786@users.noreply.github.com>
2022-09-23 02:44:20 +0200

    doing linreq

--- R/lin_req.R
+++ R/lin_req.R
@@ -1,5 +1,15 @@
 
 linreq <- function(formula, data) {
+  modelMatrixX <- model.matrix(formula, data)
+  modelMatrixY <- data[all.vars(formula)[1]]
+  head(modelMatrix)
+
+  regressionsCoefficients <- (inv(t(modelMatrixX))) %*% (t(modelMatrixX) %*% modelMatrixY)
+
+  fittedValues <- modelMatrixX %*% regressionsCoefficients
+
+  residuals <-
+
   LinreqClass <- setRefClass("linreq", fields = list(balance = "numeric"))
   a <- LinreqClass$new(balance = 100)
   return(a)
