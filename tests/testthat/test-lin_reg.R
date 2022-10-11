169fbc40f5472b7a7af89a487ab1e7a8f10587ca
arfan5191 <94747470+arfan5191@users.noreply.github.com>
2022-10-11 19:17:38 +0200

    tests added

--- tests/testthat/test-lin_reg.R
+++ tests/testthat/test-lin_reg.R
@@ -33,13 +33,3 @@ testthat::test_that("coef() method works", {
 
   testthat::expect_true(all(round(unname(linreg_mod$coef()),2) %in% c(-2.52, -1.34, 1.78)))
 })
-
-
-# testthat::test_that("summary() method works", {
-#   linreg_mod <- linreg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
-#
-#   testthat::expect_output(linreg_mod$summary(), "\\(Intercept\\)( )*-2.5[0-9]*( )*0.5[0-9]*( )*-4.4[0-9]*( )*.*( )*\\*\\*\\*")
-#   testthat::expect_output(linreg_mod$summary(), "Sepal.Width( )*-1.3[0-9]*( )*0.1[0-9]*( )*-10.9[0-9]*( )*.*( )*\\*\\*\\*")
-#   testthat::expect_output(linreg_mod$summary(), "Sepal.Length( )*1.7[0-9]*( )*0.0[0-9]*( )*27.5[0-9]*( )*.*( )*\\*\\*\\*")
-#   testthat::expect_output(linreg_mod$summary(), "Residual standard error: 0.6[0-9]* on 147 degrees of freedom")
-# })
